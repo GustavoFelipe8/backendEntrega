733e8ecfd63756fb636b8b0744e3d09b76e80c70
GustavoFelipe8 <gustavofelipe8888@gmail.com>
2024-07-01 22:48:54 -0300

    1.0.0

--- index.js
+++ index.js
@@ -28,7 +28,66 @@ app.get("/", (req, res) => {
     res.send("Ok – Servidor disponível.");
 });
 
+app.get("/usuario/:id", (req, res) => {
+    try {
+        console.log("Rota: usuario/" + req.params.id);
+        client.query(
+            "SELECT * FROM Usuarios WHERE id = $1", [req.params.id],
+            (err, result) => {
+                if (err) {
+                    return console.error("Erro ao executar a qry de SELECT id", err);
+                }
+                res.send(result.rows);
+            }
+        );
+    } catch (error) {
+        console.log(error);
+    }
+});
 
+app.post("/usuario", (req, res) => {
+    try {
+        console.log("Alguém enviou um post com os dados:", req.body);
+        const { cpf, nome, cep, numero, complemento, produto } = req.body;
+        client.query(
+            "INSERT INTO Usuarios (cpf, nome, cep, numero, complemento, produto) VALUES ($1, $2, $3, $4, $5, $6) RETURNING * ", [cpf, nome, cep, numero, complemento, produto],
+            (err, result) => {
+                if (err) {
+                    return console.error("Erro ao executar a qry de INSERT", err);
+                }
+                const { id } = result.rows[0];
+                res.setHeader("id", `${id}`);
+                res.status(201).json(result.rows[0]);
+                console.log(result);
+            }
+        );
+    } catch (erro) {
+        console.error(erro);
+    }
+});
+
+app.put("/usuario/:id", (req, res) => {
+    try {
+        console.log("Alguém enviou um update com os dados:", req.body);
+        const id = req.params.id;
+        const { cpf, nome, cep, numero, complemento, produto } = req.body;
+        client.query(
+            "UPDATE Usuarios SET cpf=$1, nome=$2, cep=$3, numero=$4, complemento=$5, produto=$6 WHERE id =$7 ",
+            [cpf, nome, cep, numero, complemento, produto, id],
+            function (err, result) {
+                if (err) {
+                    return console.error("Erro ao executar a qry de UPDATE", err);
+                } else {
+                    res.setHeader("id", id);
+                    res.status(202).json({ "identificador": id });
+                    console.log(result);
+                }
+            }
+        );
+    } catch (erro) {
+        console.error(erro);
+    }
+});
 
 //Final do arquivo
 app.listen(config.port, () =>
